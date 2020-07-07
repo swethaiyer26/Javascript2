bb559f2a382b8e2fd0c6be7cc7d07c5e58c4a15c
Swetha Janardhan <swethaiyer26@gmail.com>
2020-07-07 17:59:13 +0530

    Add files via upload

---
+++ Object_es5.js
@@ -0,0 +1,18 @@
+obj={
+    sayHello(){
+        console.log("hello");
+
+    }
+}
+
+// obj={
+//     sayHello:function() {
+//         console.log("hello");
+//     }
+// }
+
+ obj.sayHello();
+
+ var arr=[1,2,3];
+ var [a,b,c]=arr;
+ console.log(a,b,c);
