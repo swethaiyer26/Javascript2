ed613a9ff062765602509684d9cdcdc35ba4f646
Swetha Janardhan <swethaiyer26@gmail.com>
2020-07-07 17:59:57 +0530

    Add files via upload

---
+++ Es5.js
@@ -0,0 +1,49 @@
+// function logFirst(num=0,data=['cats']){
+//     console.log(`i own ${num}  ${data[0]}`);
+// }
+// logFirst(3,['dogs']);
+
+
+// var middle=[3,4];
+// var arr=[1,2,...middle,5,6];
+// console.log(arr);
+
+
+// var arr1=[1,2,3];
+// var arr2=[...arr1];
+// arr2.push(4);
+// console.log(arr1);
+// console.log(arr2);
+
+
+// let arr3=['a','b','c'];
+// let iterator=arr.keys();
+
+// iterator.next();
+// iterator.next();
+// iterator.next();
+// iterator.next();
+
+// let arr4 = ['a','b','c'];
+// let iterator1 = arr4.values();
+
+// iterator1.next().value();
+// iterator1.next().value();
+// iterator1.next().value();
+// iterator1.next().value();
+
+// let arr = ['a','b','c'];
+// let iterator = arr.entries();
+
+// iterator.next().value();
+// iterator.next().value();
+// iterator.next().value();
+// iterator.next().value();
+
+var x=6;
+var y='hello';
+
+var obj={
+    x,
+    y
+}
